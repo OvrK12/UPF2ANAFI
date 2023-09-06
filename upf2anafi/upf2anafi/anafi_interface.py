11c05161b70db3b61df6e6b8b0af88b0577b9df2
OvrK12 <krauso@tcd.ie>
2023-09-06 17:52:55 +0200

    Added remote action for takeoff

--- upf2anafi/upf2anafi/anafi_interface.py
+++ upf2anafi/upf2anafi/anafi_interface.py
@@ -1,8 +1,10 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.action import CancelResponse, GoalResponse
 
 from rclpy.action import ActionServer
 from nav2_msgs.action import NavigateToPose
+from std_srvs.srv import Trigger, SetBool
 
 class AnafiInterfaceNode(Node):
     """
@@ -24,12 +26,47 @@ class AnafiInterfaceNode(Node):
 
         # create action servers
         self._fly_action_server = ActionServer(self, NavigateToPose, self._drone_prefix + "navigate_to_pose", self.execute_callback)
-        self._takeoff_action_server = ActionServer(self, NavigateToPose, self._drone_prefix + "takeoff", self.execute_callback)
+        self._takeoff_action_server = ActionServer(self, NavigateToPose,
+                                                   self._drone_prefix + "takeoff",
+                                                   self.execute_takeoff_callback,
+                                                   goal_callback=self.goal_callback,
+                                                   cancel_callback=self.cancel_callback)
         self._landing_action_server = ActionServer(self, NavigateToPose, self._drone_prefix + "landing", self.execute_callback)
 
+        # create clients
+        self._arm_drone_client = self.create_client(SetBool, '/anafi/drone/arm')
+        self._drone_takeoff_client = self.create_client(Trigger, '/anafi/drone/takeoff')
+
         
-    def execute_callback(self):
+    def execute_callback(self, goal_handle):
         None
+
+    def goal_callback(self, goal_request):
+        # Accepts or rejects a client request to begin an action
+        self.get_logger().info('Received goal request')
+        self.goal = goal_request
+        return GoalResponse.ACCEPT
+
+    def cancel_callback(self, goal_handle):
+        # Accepts or rejects a client request to cancel an action
+        self.get_logger().info('Received cancel request')
+        return CancelResponse.ACCEPT
+
+    def execute_fly_callback(self, goal_handle):
+        None
+
+    def execute_takeoff_callback(self, goal_handle):
+        srv = SetBool.Request()
+        future = self._arm_drone_client.call_async(srv)
+        rclpy.spin_until_future_complete(self, future)
+
+        srv = Trigger.Request()
+        future = self._drone_takeoff_client.call_async(srv)
+        rclpy.spin_until_future_complete(self, future)
+        goal_handle.succeed()
+
+        result = NavigateToPose.Result()
+        return result
 
 def main(args=None):
     rclpy.init(args=args)
